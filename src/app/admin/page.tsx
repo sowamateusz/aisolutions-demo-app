8dd028c0b66a034a93af0d0ae33d43d6ab764aa3
Mateusz Sowa <sowa@hush.ai>
2024-12-14 20:09:08 +0100

    update app

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,20 +5,21 @@ import { Link } from "aisolutions-react-components";
 
 const AdminHome = () => {
   const { user } = useUser();
-  if (!user) return null;
-  if (user)
-    return (
-      <div className="p-5">
-        <h1 className="text-lg">Demo App \ Admin</h1>
+
+  return (
+    <div className="p-5">
+      <h1 className="text-lg">Demo App \ Admin</h1>
+      {user && (
         <div className="flex gap-5 mt-5">
           <Link href="/" text="Home" variant="secondary" />
           <Link href="/api/auth/me" text="Me" variant="secondary" />
           <Link href="/api/auth/logout" text="Logout" variant="secondary" />
         </div>
-        <hr className="my-10" />
-        <div className="mt-10">Super Secure Private Page</div>
-      </div>
-    );
+      )}
+      <hr className="my-10" />
+      <div className="mt-10">Super Secure Private Page</div>
+    </div>
+  );
 };
 
 export default AdminHome;
